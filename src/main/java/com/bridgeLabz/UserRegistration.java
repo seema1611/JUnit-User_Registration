e859a075506ea27626d13a0bbd57401b0c4b5856
seema1611 <seemarajpure16@gmail.com>
2020-04-21 22:27:38 +0530

    MODIFIED: Use constants for pattern

--- src/main/java/com/bridgeLabz/UserRegistration.java
+++ src/main/java/com/bridgeLabz/UserRegistration.java
@@ -3,23 +3,29 @@ package com.bridgeLabz;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+    String firstNamePattern="^[A-Z]{1}[a-z]{3,}$";
+    String lastNamePattern="^[A-Z]{1}[a-z]{3,}$";
+    String emailPattern="^[A-Za-z]{3,}([.|+|_|-]?[A-Za-z0-9]+)?[@][A-Za-z0-9]+[.][A-Za-z]{2,4}([.][A-Za-z]{2,4}+)?$";
+    String mobileNumberPattern="^[0-9]{1,3}[ ][1-9]{1}[0-9]{9}$";
+    String passwordPattern="^(?=[^@|#|$|%|!|&]*[@|#|$|%|!|&][^@|#|$|%|!|&]*$)(?=.*[0-9])(?=.*[A-Z])[A-Za-z0-9@#$%!&]{8,}$";
+
     public boolean validateFirstName(String firstName) {
-        return Pattern.matches("^[A-Z]{1}[a-z]{3,}$",firstName);
+        return Pattern.matches(firstNamePattern,firstName);
     }
 
     public boolean validateLastName(String lastName) {
-        return Pattern.matches("^[A-Z]{1}[a-z]{3,}$",lastName);
+        return Pattern.matches(lastNamePattern,lastName);
     }
 
     public boolean validateEmail(String email) {
-        return Pattern.matches("^[A-Za-z]{3,}([.|+|_|-]?[A-Za-z0-9]+)?[@][A-Za-z0-9]+[.][A-Za-z]{2,4}([.][A-Za-z]{2,4}+)?$",email);
+        return Pattern.matches(emailPattern,email);
     }
 
     public boolean validateMobileNumber(String mobileNumber) {
-        return Pattern.matches("^[0-9]{1,3}[ ][1-9]{1}[0-9]{9}$",mobileNumber);
+        return Pattern.matches(mobileNumberPattern,mobileNumber);
     }
 
     public boolean validatePassword(String password) {
-        return Pattern.matches("^^(?=[^@|#|$|%|!|&]*[@|#|$|%|!|&][^@|#|$|%|!|&]*$)(?=.*[0-9])(?=.*[A-Z])[A-Za-z0-9@#$%!&]{8,}$",password);
+        return Pattern.matches(passwordPattern,password);
     }
 }
